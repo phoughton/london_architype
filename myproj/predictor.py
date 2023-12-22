be211b84a288a8aa314ef23a94cea9ea0db560e8
phoughton <pete@investigatingsoftware.co.uk>
2023-12-22 19:44:37 +0000

    Add valid image URL to use

--- myproj/predictor.py
+++ myproj/predictor.py
@@ -2,7 +2,7 @@ from gradio_client import Client
 
 client = Client("https://phoughton-london-architecture.hf.space/--replicas/8r8d7/")
 result = client.predict(
-		https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png,	# filepath  in 'the_image' Image component
+		"https://raw.githubusercontent.com/phoughton/london_architype/a646ed8efe35b2a5f1617e7531558ad3d797de9a/images/tudor_1.jpg",	# filepath  in 'the_image' Image component
 							api_name="/predict"
 )
 print(result)
